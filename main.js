eb0ece6a3af37663303efed4bf1c7d02e006bf5c
jaspreet548 <jaspreet.singh548@gmail.com>
2021-06-24 09:34:15 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var url = "https://www.canada.ca/en/immigration-refugees-citizenship/corporate/mandate/policies-operational-instructions-agreements/ministerial-instructions/express-entry-rounds.html";
+
+// $.get( url , function( data ) {
+//    console.log(data);
+//    debugger
+//   });
+
+  $.get( "./data.txt" , function( data ) {
+    console.log(data);
+    debugger
+   });
