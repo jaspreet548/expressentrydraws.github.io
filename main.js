656bcd8077f0a401699d296500d31ef6d8fc552d
jaspreet548 <jaspreet.singh548@gmail.com>
2021-06-24 16:40:11 +0530

    add

--- main.js
+++ main.js
@@ -16,4 +16,31 @@ var url = "https://www.canada.ca/en/immigration-refugees-citizenship/corporate/m
     var dom_nodes = $($.parseHTML(table));
     console.log(dom_nodes);
 
+    var jsonData = {
+      "immigrationPrograms": {
+        "types" : [	
+          "All",
+          "Canadian Experience Class",
+          "Provincial Nominee Program",
+          "Federal Skilled Worker (No program specified)",
+          "Federal Skilled Trades"
+        ],
+        "totalDraws" : [
+        ]
+      },
+      "draws":[
+        {"drawNo": 1, "details":"John", "date":"Doe", "immigrationProgram":"Doe", "invitationsIssued":"", "crsScore":"", "programsCovered": ""},
+        {"details":"John", "date":"Doe", "immigrationProgram":"Doe", "invitationsIssued":"", "crsScore":"", "programsCovered": ""}
+      ],
+      "years" : [
+        
+      ]
+    };
+
+    var drawTotalCount = dom_nodes[0].rows.length;
+
+    $(dom_nodes[0].rows).each(function( index ) {
+      console.log( index + ": " + $( this ).text() );
+    });
+
    });
